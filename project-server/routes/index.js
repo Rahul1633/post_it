a9b1f0feb46d8b9b135ec4f911739307f42d18e5
Abdus Samad Khwaja <abdussamad407@gmail.com>
2023-09-29 22:19:03 +0530

    minor style changes

--- project-server/routes/index.js
+++ project-server/routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const request = require('request');
 const router = express.Router();
 const multer = require('multer');
 const { Post } = require('../../models/schema');
+
+const apiOptions = {
+    server: "http://localhost:8000"
+};
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = 'https://post-it-three-kappa.vercel.app';
+};
 
 
 const storage = multer.diskStorage({
@@ -15,9 +23,38 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+const renderHomepage = (req, res, responseBody) => {
+    res.render('index', {
+        posts: responseBody
+    });
+};
+
 router.get('/', (req, res, next) => {
-  res.render('index');
+    const path = '/posts';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+        qs: {}
+    };
+    request(
+        requestOptions,
+        (err, response, body) => {
+            renderHomepage(req, res, body);
+        }
+    );
   // res.sendFile('login.html', { root: 'project-server/views' });
+});
+
+router.get('/posts', async(req, res) => {
+    try {
+        const data = await Post.find({})
+            .then(function (posts) {
+                res.status(200).json(posts);
+            })
+    } catch (err) {
+        res.status(404).json(err);
+    }
 });
 
 router.post('/post/new', upload.single('postImg'), async (req, res) => {
