144e063bb7187d2608be057dae64ce3f9930ffa5
Rahul1633 <rahulgoud16032003@gmail.com>
2023-10-04 21:43:45 +0530

    initial commit

--- project-server/routes/users.js
+++ project-server/routes/users.js
@@ -53,17 +53,14 @@ router.get( '/login', (req, res) => {
     res.render('login')
 });
 
-router.post ( '/register' , async (req, res) => { 
-    const {name, email, password} = req.body;  
-    console.log(req.body)
+router.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
     try {
-        const user = await User.create({ name: name, email: email, password: password });
+        const user = await User.create({ name, email, password });
         const token = createToken(user._id);
-        res.cookie('jwt', token, {httpOnly: true,maxAge: maxAge * 1000});
-        res.status(200).json({user: user._id});
-
-    } catch(err) {
-        
+        // Send user data in the response
+        res.status(200).json({ user });
+    } catch (err) {
         const errors = handleErrors(err);
         res.status(400).json(errors);
     }
